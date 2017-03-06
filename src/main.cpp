875be4792799d2dd389d8cd81b1e1493c8013b29
Kevin Díaz Marrero <alu0100880625@ull.edu.es>
2017-03-06 20:50:36 +0000

    Mostrar inicial solo en debug

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 void usage(void)
 {
   std::cout << "usage: ./bin/hanoi_tower num_discos debug [cyclic]" << std::endl;
-  
+
 }
 
 int main(int argc, char *argv[])
 {
-  bool ciclico = 0;
+  bool ciclico = 0, debug;
   unsigned tiempo_inicial, tiempo_final;
   double tiempo_ejecucion;
   hanoi_t* hanoi;
@@ -23,15 +23,16 @@ int main(int argc, char *argv[])
     hanoi = new hanoi_ciclico_t(atoi(argv[1]));
   else
     hanoi = new hanoi_t(atoi(argv[1]));
-  
-  std::cout << *hanoi <<std::endl;
-  
+  debug = argv[2][0] == '1';
+  if(debug)
+    std::cout << *hanoi <<std::endl;
+
   tiempo_inicial = clock();
-  hanoi->resolver(argv[2][0] == '1');
+  hanoi->resolver(debug);
   tiempo_final = clock();
-  
+
   tiempo_ejecucion = (double (tiempo_final - tiempo_inicial) / CLOCKS_PER_SEC);
-  
+
   std::cout << "Movimientos necesarios: " << hanoi->get_numero_movimientos() <<std::endl;
   std::cout << "Tiempo de ejecución: " << tiempo_ejecucion << std::endl;
 }
