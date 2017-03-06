eeff2414c9b62b3bf086dc539cbf73bcccff55e3
missigno <kururu999kdm@gmail.com>
2017-03-06 17:02:52 +0000

    se ha añadido un medidor de tiempo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "../include/hanoi_ciclico_t.hpp"
-
+#include <ctime>
 void usage(void)
 {
   std::cout << "usage: ./bin/hanoi_tower num_discos debug [cyclic]" << std::endl;
@@ -9,6 +9,8 @@ void usage(void)
 int main(int argc, char *argv[])
 {
   bool ciclico = 0;
+  unsigned tiempo_inicial, tiempo_final;
+  double tiempo_ejecucion;
   hanoi_t* hanoi;
   if(argc < 3)
   {
@@ -23,6 +25,13 @@ int main(int argc, char *argv[])
     hanoi = new hanoi_t(atoi(argv[1]));
   
   std::cout << *hanoi <<std::endl;
+  
+  tiempo_inicial = clock();
   hanoi->resolver(argv[2][0] == '1');
+  tiempo_final = clock();
+  
+  tiempo_ejecucion = (double (tiempo_final - tiempo_inicial) / CLOCKS_PER_SEC);
+  
   std::cout << "Movimientos necesarios: " << hanoi->get_numero_movimientos() <<std::endl;
+  std::cout << "Tiempo de ejecución: " << tiempo_ejecucion << std::endl;
 }
